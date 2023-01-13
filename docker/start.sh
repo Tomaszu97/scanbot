358087b45a9655beba2b58547f9713c533089f3c
Tomasz Jakubowski <tjakubowski@plume.com>
2023-01-13 18:15:51 +0100

    Added presets to start helper script

--- docker/start.sh
+++ docker/start.sh
@@ -5,5 +5,16 @@
 xhost +local:root
 
 # nvidia on-demand on linux:
-__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia \
-docker-compose up $@
+nvidia_offload="__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia "
+
+# run presets
+gazebo_with_gamepad="roscore rosconsole gmapping rviz gazebo gamepad gamepad_conv"
+hw_with_gamepad="roscore rosconsole gmapping rviz gamepad gamepad_conv"
+
+if [ "$1" == "gb" ] ; then
+    env $nvidia_offload docker-compose up $gazebo_with_gamepad
+elif [ "$1" == "hw" ] ; then
+    env $nvidia_offload docker-compose up $hardware_with_gamepad
+else
+    env $nvidia_offload docker-compose up $@
+fi
