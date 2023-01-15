b01298c24c149ac1ce3a9513ddc7fe4b509336cf
Tomasz Jakubowski <tjakubowski@plume.com>
2023-01-15 21:05:24 +0100

    Split launch files into gazebo and hardware

--- docker/start.sh
+++ docker/start.sh
@@ -1,20 +1,38 @@
-#!/bin/bash -axe
+#!/bin/bash -ae
 
 # remember to rebuild images with "docker-compose build" if required!
 
-xhost +local:root
+xhost +local:root > /dev/null
 
 # nvidia on-demand on linux:
 nvidia_offload="__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia "
 
 # run presets
-gazebo_with_gamepad="roscore rosconsole gmapping rviz gazebo gamepad gamepad_conv"
-hw_with_gamepad="roscore rosconsole gmapping rviz gamepad gamepad_conv"
+gazebo_with_gamepad="
+roscore_gazebo
+rosconsole
+gmapping
+rviz
+gazebo
+gamepad
+gamepad_conv"
+hardware_with_gamepad="
+roscore_hardware
+rosconsole
+gmapping
+rviz
+gamepad
+gamepad_conv"
 
 if [ "$1" == "gb" ] ; then
     env $nvidia_offload docker-compose up $gazebo_with_gamepad
 elif [ "$1" == "hw" ] ; then
+    # REMEMBER TO SET  /use_sim_time to FALSE
     env $nvidia_offload docker-compose up $hardware_with_gamepad
 else
-    env $nvidia_offload docker-compose up $@
+    echo "specify container collection to run, available:"
+    echo "  gb - gazebo"
+    echo "  hw - hardware"
+    echo "example: ./start.sh gb"
+    exit 1
 fi
