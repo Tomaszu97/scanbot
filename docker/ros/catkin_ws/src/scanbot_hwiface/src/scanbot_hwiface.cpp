e4cc2c6aec404db315096f7f1630750977e9cf86
Tomasz Jakubowski <tjakubowski@plume.com>
2023-01-15 22:32:05 +0100

    Removed commented out code

--- docker/ros/catkin_ws/src/scanbot_hwiface/src/scanbot_hwiface.cpp
+++ docker/ros/catkin_ws/src/scanbot_hwiface/src/scanbot_hwiface.cpp
@@ -81,33 +81,3 @@ int main(int argc, char **argv)
        rate.sleep();
     }
 }
-
-
-
-
-
-
-//#include <ROBOT_hardware_interface/ROBOT_hardware_interface.h>
-//int main(int argc, char** argv)
-//{
-//    ros::init(argc, argv, "ROBOT_hardware_interface");
-//    ros::CallbackQueue ros_queue;
-//
-//    ros::NodeHandle nh;
-//    nh.setCallbackQueue(&ros_queue);
-//    ROBOT_hardware_interface::ROBOTHardwareInterface rhi(nh);
-//
-//    ros::MultiThreadedSpinner spinner(0);
-//    spinner.spin(&ros_queue);
-//    return 0;
-//}
-
-
-//int main(int argc, char **argv) {
-//  ros::init(argc, argv, "scanbot_hwiface");
-//  ros::NodeHandle n;
-//  ros::spin();
-//  return 0;
-//}
-
-
