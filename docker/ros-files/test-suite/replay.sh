c6a304ba7c545d090e454b5699a1924c304047b4
Tomasz Jakubowski <tjakubowski@plume.com>
2023-02-18 19:53:12 +0100

    Updated and tested child process killing handling in test and replay scripts

--- docker/ros-files/test-suite/replay.sh
+++ docker/ros-files/test-suite/replay.sh
@@ -4,10 +4,22 @@ BAG="$1"
 BAG_REPLAY_RATE="$2"
 test "$#" == "2"
 
-trap 'pkill -P $$' SIGINT SIGTERM EXIT
+replay_cleanup()
+{
+    : "replay: cleaning up"
+    pkill -P $$ -SIGINT
+    exit $1
+}
 
+trap "replay_cleanup 0" SIGINT SIGTERM
+trap "replay_cleanup -1" ERR SIGHUP
+
+: "replay: starting tf remapper"
 # hide frame transforms originally published by slam node
 # set this as rosparam due to bug ros/ros_comm#1498 
 rosparam set '/tf_remapper/mappings' '[{"old": "map", "new": ""}]'
 rosrun tf_remapper_cpp tf_remap &
+
+: "replay: playing rosbag"
 rosbag play --clock -r "$BAG_REPLAY_RATE" "${BAG}.bag" tf:=tf_old
+replay_cleanup 0
--- docker/ros-files/test-suite/test.sh
+++ docker/ros-files/test-suite/test.sh
@@ -9,23 +9,30 @@ TEST_PARAM_STEP="$6"
 TEST_PARAM_MAX="$7"
 test "$#" == "7"
 
+cleanup()
+{
+    : "test: cleaning up"
+    pkill -P $$ -SIGINT
+    exit $1
+}
+
+trap "cleanup 0" SIGINT SIGTERM
+trap "cleanup -1" ERR SIGHUP
+
 for curr_param_value in $(seq "$TEST_PARAM_MIN" "$TEST_PARAM_STEP" "$TEST_PARAM_MAX")
 do
-    : "running test for param $curr_param_value"
-    : "starting algorithm $ALGORITHM"
+    : "test: running test for param $curr_param_value"
+    : "test: starting algorithm $ALGORITHM"
     env "${ALGORITHM}_${TEST_PARAM_NAME}=${curr_param_value}" ./run-algorithm.sh "${ALGORITHM}" &
-    algo_pid="$!"
 
-    : "replaying bag $BAG"
+    : "test: replaying bag $BAG"
     ./replay.sh "$BAG" "$BAG_REPLAY_RATE"
 
-    : "saving map as image"
+    : "test: saving map as image"
     curr_dirname="test/${ALGORITHM}/${BAG}_x${BAG_REPLAY_RATE}/${TEST_PARAM_NAME}/"
     mkdir -p "$curr_dirname"
     ./save-map.sh "${curr_dirname}/${curr_param_value}"
 
-    : "killing algorithm"
-    rosnode list | grep "$ALGORITHM" | xargs -n1 rosnode kill
-    while ps -p "$algo_pid" ; do sleep 1 ; done
-
+    : "test: killing algorithm"
+    rosnode list | grep "$ALGORITHM" | xargs -n 1 rosnode kill
 done
