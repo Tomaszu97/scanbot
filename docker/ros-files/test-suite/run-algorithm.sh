d0164bd540bb62487ad668f74f4eccb9021678e2
Tomasz Jakubowski <tjakubowski@plume.com>
2023-02-19 19:56:44 +0100

    Minor fixes in run angorithm script regarding logging and file existence checks

--- docker/ros-files/test-suite/run-algorithm.sh
+++ docker/ros-files/test-suite/run-algorithm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ae
 
 ALGORITHM="$1"
-test "$#" == "1"
+test "$#" == "1" || { echo "run-algorithm: check parameters" ; exit 1 ; }
 
 echo "run-algorithm: sourcing parameters"
 test -e "${ALGORITHM}.overrides" && source "${ALGORITHM}.overrides"
-test -e "${ALGORITHM}.overrides" && source "${ALGORITHM}.defaults"
+test -e "${ALGORITHM}.defaults" && source "${ALGORITHM}.defaults"
 
 echo "run-algorithm: exec algorithm"
 exec "./${ALGORITHM}.run"
